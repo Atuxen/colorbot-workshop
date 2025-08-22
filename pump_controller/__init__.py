e8b8afe15e691d3fff3f2027642db71f27b524b5
Andreas <andreastuxen@gmail.com>
2025-08-22 17:18:09 +0200

    WIP

--- pump_controller/__init__.py
+++ pump_controller/__init__.py
@@ -3,6 +3,10 @@ from .silico_pump_controller import SilicoPumpController
 from .serial_help import list_serial_ports, get_serial_port
 from .colors_help import visualize_rgb, visualize_candidates
 from .utils import read_logfile, write_to_logfile
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 __all__ = [
     "PumpController",
@@ -13,4 +17,6 @@ __all__ = [
     "visualize_candidates",
     "read_logfile",
     "write_to_logfile",
+    "np",
+    "plt"
 ]
